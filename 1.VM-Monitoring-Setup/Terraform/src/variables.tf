9d991ce63c32d1e9466e7fd427cdd2a1edce67d1
Daniel Ayuga <danielayugachacon@outlook.com>
2026-03-31 11:15:31 +0200

    Create variables.tf

---
+++ 1.VM-Monitoring-Setup/Terraform/src/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  type        = string
+  description = "rg-vmmon-setup"
+}
+
+variable "location" {
+  type        = string
+  description = "Spain Central"
+  default     = "spaincentral"
+}
