efe0c8062574a82a9bd1678af405da53a9f7555d
Daniel <danielayugachacon@outlook.com>
2026-04-01 11:31:57 +0200

    chore: recover terraform config files from save/my-local-main (exclude .terraform binaries)

--- 1.VM-Monitoring-Setup/Terraform/src/main.tf
+++ 1.VM-Monitoring-Setup/Terraform/src/main.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+module "networking" {
+  source = "./modules/networking"
+
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  vnet_name   = var.vnet_name
+  subnet_name = var.subnet_name
+  nsg_name    = var.nsg_name
+}
